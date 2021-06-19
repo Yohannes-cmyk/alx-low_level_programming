59704a883823ef8b073251914e90d602e92c0bdd
Yohannes <ytaye23@gmail.com>
2021-06-19 08:28:30 -0700

    Task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int a = 0;
+	
+	for(;a<10;a++)
+	{
+		putchar(a);	
+	}
+	putchar(10);
+	return (0);
+}
