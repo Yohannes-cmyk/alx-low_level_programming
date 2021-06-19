e9b1121d099b425d5d1eb9308efb42731f8f85d8
Yohannes <ytaye23@gmail.com>
2021-06-19 07:55:20 -0700

    Task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+  int a;
+	int b;
+	
+	for (a='a';a<='z';a++)
+	{
+		putchar(a);
+	}
+	for (b='A';b<='Z';b++)
+		putchar(b);
+		
+	putchar(10);
+	return (0);
+}
