0e2ef5423dcf2d63b5c7e50335bc3eb7bcf4dfe0
Yohannes <ytaye23@gmail.com>
2021-06-19 08:19:02 -0700

    Updating task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,4 @@
+File Edit Options Buffers Tools C Help
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -5,12 +6,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	
-	for (n='a';n<='z';n++){
-		if (n != 'q' && n != 'e')
-			putchar(n);
-	}
-	putchar(10);
-	return (0);
+        int n;
+
+        for (n='a';n<='z';n++){
+                if (n!='q' && n!='e')
+                        putchar(n);
+        }
+        putchar(10);
+        return (0);
 }
