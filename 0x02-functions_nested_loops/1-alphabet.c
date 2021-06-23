48c2dfcbc8a08ca8e8807d6f2fa9d98af2798415
Yohannes <ytaye23@gmail.com>
2021-06-23 10:46:39 -0700

    fixing betty.doc task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
- * main - Printing holbert using loop and _putchar
- *
+ * main - Printing the lower case of the alphabets
+ *@n = characters 
+ *on every loop of the while it prints out the letter and increment by one until the argument is false (z<=z)
  * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
     int n = 'a';
+    
+
     while(n <= 'z')
     {
         _putchar(n);
