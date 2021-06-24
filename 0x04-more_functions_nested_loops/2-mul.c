592ca77af53031b98cba361cee361108da4690cd
Yohannes <ytaye23@gmail.com>
2021-06-24 09:48:06 -0700

    Task two completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplys two integers
+ *@num1 - integer one
+ *@num2 - integer two 
+ *Returns - The product of num1 and num2. 
+ */
+ 
+int mul(int a, int b)
+{
+        int result;
+        
+        result= a*b;
+        return(result);
+}
